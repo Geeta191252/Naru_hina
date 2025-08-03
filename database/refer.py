6690b47674ba4e906a7406389ed65dba1ff42887
⌯ Ꭺɴᴏɴʏᴍᴏᴜꜱ | ×͜× | <155171427+NBBotz@users.noreply.github.com>
2025-08-03 22:36:02 +0530

    SilentXBotz

--- database/refer.py
+++ database/refer.py
@@ -1,9 +1,5 @@
 import pymongo
 from info import DATABASE_URI, DATABASE_NAME
-import logging
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
 
 myclient = pymongo.MongoClient(DATABASE_URI)
 mydb = myclient[DATABASE_NAME]

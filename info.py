fc93ef60f19e541eeef20f8f3fe2155549503f6e
Geeta191252 <Chetanmehar599@gmail.com>
2025-09-30 07:55:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'media_search')
 API_ID = int(environ.get('API_ID', '25059287'))
 API_HASH = environ.get('API_HASH', '5e7701953107a273724b07f2beaf8f17')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+BOT_TOKEN = environ.get('BOT_TOKEN', "8497446543:AAHNbczuKt_bEkg_o_RIL1OG4haWQNJp7mg")
 
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))

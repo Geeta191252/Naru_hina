61a52da6407ba4f2b3fe32e8b723f6466eeebbbc
Geeta191252 <Chetanmehar599@gmail.com>
2025-10-03 22:38:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,8 +43,8 @@ support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002517228726')
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://chetan:chetan1@cluster0.agvz0cd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Cluster0')
+DATABASE_NAME = environ.get('DATABASE_NAME', "mehar")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mehar')
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "False"), False) # Type True For Turn On MULTIPLE DB FUNTION 

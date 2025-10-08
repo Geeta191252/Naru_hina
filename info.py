89e2b4c7772b4a9e34e97fda2457a0dd7a75e190
Geeta191252 <Chetanmehar599@gmail.com>
2025-10-08 20:57:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,9 +47,9 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "mehar")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mehar')
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
-MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "True"), false) # Type True For Turn On MULTIPLE DB FUNTION 
+MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "false"), false) # Type True For Turn On MULTIPLE DB FUNTION 
 DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://chetan2:chetan2@cluster0.urotykv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_CHANGE_LIMIT = int(environ.get('DB_CHANGE_LIMIT', "206")) 
+DB_CHANGE_LIMIT = int(environ.get('DB_CHANGE_LIMIT', "432")) 
 
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movie_hd_hub15')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+tgPf04FXMOllMWVl')

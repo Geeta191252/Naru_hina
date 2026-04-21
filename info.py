78b68041199b1c2cde2b93ef49b2352ca4e17632
Geeta191252 <Chetanmehar599@gmail.com>
2026-04-22 01:01:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mehar')
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "True"), True) # Type True For Turn On MULTIPLE DB FUNTION 
 DATABASE_URI2 = environ.get('DATABASE_URI2', "")
-DB_CHANGE_LIMIT = int(environ.get('DB_CHANGE_LIMIT', "260")) 
+DB_CHANGE_LIMIT = int(environ.get('DB_CHANGE_LIMIT', "257")) 
 
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movie_hd_hub15')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+tgPf04FXMOllMWVl')

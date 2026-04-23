bc6526bb2cd5e7818eec96b36cba67e75cdc13a6
Geeta191252 <Chetanmehar599@gmail.com>
2026-04-23 11:12:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,7 +60,7 @@ UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/+tgPf04FXMO
 AUTH_CHANNEL = environ.get("AUTH_CHANNEL", "-1002200226545") # add multiple channels here, separated by single space
 AUTH_REQ_CHANNEL = environ.get('AUTH_REQ_CHANNEL', '-1002926855756') # add multiple channels here, separated by single space
 
-IS_VERIFY = is_enabled('IS_VERIFY', True)
+IS_VERIFY = is_enabled('IS_VERIFY', False)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '100'))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-100'))
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
